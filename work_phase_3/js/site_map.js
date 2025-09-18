4b598f8a2db6ef4cd14d9cdca278919b8c12b370
isaksjobergUQ <i.sjoeberg@student.uq.edu.au>
2025-09-18 12:09:57 +1000

    half working site map, needs updates on the Interactive map section

---
+++ work_phase_3/js/site_map.js
@@ -0,0 +1,32 @@
+/**
+ * IMPORTS
+ * Keep track of external modules being used
+ * */
+import { initAccordion } from './modules/accordion.js';
+
+/**
+ * CONSTANTS
+ * Define values that don't change e.g. page titles, URLs, etc.
+ * */
+const ACCORDION_ID = 'site-map-accordion';
+
+/**
+ * VARIABLES
+ * Define values that will change e.g. user inputs, counters, etc.
+ * */
+
+/**
+ * FUNCTIONS
+ * Group code into functions to make it reusable
+ * */
+function initPage() {
+    initAccordion(ACCORDION_ID);
+}
+
+/**
+ * EVENT LISTENERS
+ * The code that runs when a user interacts with the page
+ * */
+
+// when the page fully loads
+document.addEventListener('DOMContentLoaded', initPage);
