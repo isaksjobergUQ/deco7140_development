db32a9e46b5becfb2d2e206029e1b76326c3fe2f
isaksjobergUQ <i.sjoeberg@student.uq.edu.au>
2025-10-06 11:06:30 +1000

    Ethical response: Focused on the most important aspects disucessd from WP2, also thoiught about what my test subjects answered as well as created a draft for stakeholder

---
+++ work_phase_3/js/ethical_response.js
@@ -0,0 +1,33 @@
+/**
+ * IMPORTS
+ * Keep track of external modules being used
+ * */
+
+/**
+ * CONSTANTS
+ * Define values that don't change e.g. page titles, URLs, etc.
+ * */
+
+/**
+ * VARIABLES
+ * Define values that will change e.g. user inputs, counters, etc.
+ * */
+
+/**
+ * FUNCTIONS
+ * Group code into functions to make it reusable
+ * */
+
+/**
+ * EVENT LISTENERS
+ * The code that runs when a user interacts with the page
+ * */
+
+// when the page fully loads
+document.addEventListener('DOMContentLoaded', () => {
+    // Initialize ethical response page functionality
+    console.log('Ethical Response page loaded');
+    
+    // Add any interactive elements or functionality here
+    // For example: smooth scrolling, animations, etc.
+});
