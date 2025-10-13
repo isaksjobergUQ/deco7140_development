74b10d81ec9c27b6e0ca67228f61dde9a8029152
isaksjobergUQ <i.sjoeberg@student.uq.edu.au>
2025-10-13 18:50:10 +1000

    initial Work Phase 4 commit

---
+++ work_phase_4/js/index.js
@@ -0,0 +1,8 @@
+// Set current year in footer
+document.addEventListener('DOMContentLoaded', () => {
+    const yearEl = document.getElementById('year');
+    if (yearEl) {
+        yearEl.textContent = new Date().getFullYear();
+    }
+});
+
